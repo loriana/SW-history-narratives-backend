796da0fad7c6b81544a971159bd58376599f0a8f
Maria Loriana Porumb <e1429844@student.tuwien.ac.at>
2021-04-11 16:33:13 +0200

    Read url and local path from the command line args

--- app.js
+++ app.js
@@ -1,18 +1,39 @@
 'use strict'
 
+const git = require('./utils/git_utils')
 
-const express = require('express') 
+const yargs = require('yargs/yargs')
+const { hideBin } = require('yargs/helpers')
+const argv = yargs(hideBin(process.argv)).argv
+
+const express = require('express')
 const app = express()
-const bodyParser = require("body-parser");
+const bodyParser = require("body-parser")
 let cors = require('cors')
 
 //import route
 const commitRoutes = require('./routes/commits')
 
-
 app.use(express.json())
 app.use(bodyParser.json())
 app.use(cors())
+
+
+/**
+ * Read arguments and either clone a repo at a local path
+ * Or update the variable local to the local path provided (if no url given)
+ */
+global.local = ""  //the local repo path, available globally
+
+if (argv.url && argv.local) {
+    local = argv.local
+    git.clone_repo(argv.url, local)
+} else if (!argv.url && argv.local) {
+    local = argv.local
+} else {
+    console.error("You need to specify a path to a local repo with --local=your_path")
+    console.error("Optionally, you can also specify a repo's url with --url=the_repo_url, which will clone the remote repo into at local path")
+}
 
 
 const port = process.env.port || 3030
@@ -23,9 +44,9 @@ app.listen(port, () => console.log(`Listening on port ${port}`))
 app.use('/commits', commitRoutes);
 
 
-app.use((req,res,next) => {
+app.use((req, res, next) => {
     res.sendStatus(404)
-});  
+});
 
 
 module.exports = app
--- utils/git_utils.js
+++ utils/git_utils.js
@@ -8,22 +8,42 @@ const FileType = require('file-type');
 const { NoSuchShaError, MalformedTheoryPathsError } = require('./custom_errors');
 
 var mmm = require('mmmagic'),
-Magic = mmm.Magic;
-
-
-
-var url = "https://github.com/web-engineering-tuwien/recipe-search-live-demo.git", //in case we want to clone a repo
-  //local = "/Users/lorianaporumb/Desktop/RecipePuppy" 
-  local = "/Users/lorianaporumb/Desktop/Test_repo" 
-  cloneOpts = {};
-
-
+  Magic = mmm.Magic;
 
 const exec = promisify(require('child_process').exec)
 const readFile = (fileName) => promisify(fs.readFile)(fileName/*, 'utf8'*/);
 
 
+
+
+/**
+ * Clones a remote repo at an url at a local path
+ * If the local path already exists, it performs a recursive remove before cloning
+ * @param {*} url 
+ * @param {*} local 
+ */
+async function clone_repo(url, local) {
+
+  if (fs.existsSync(local)) {
+    try {
+      fs.rmdirSync(local, { recursive: true })
+    } catch (err) {
+      console.error(`Error while deleting ${local}.`)
+    }
+  }
+
+  cloneOpts = {}
+  nodegit.Clone(url, local, cloneOpts).then(function (repo) {
+    console.log("Cloned " + path.basename(url) + " to " + repo.workdir())
+  }).catch(function (err) {
+    console.log(err)
+  });
+}
+
+
+
 async function commit_exists(sha) {
+
   const command = `git cat-file commit ${sha}`
   const access_repo = `cd ${local}`
 
@@ -35,19 +55,36 @@ async function commit_exists(sha) {
 
 }
 
-
+/**
+ * Returns the sha of the first commit
+ * If the first commit's message equals "initial commit" or "first commit", it moves on to its child
+ */
 async function get_first_commit() {
-    const first_commit = 'git rev-list --max-parents=0 HEAD'
-    const access_repo = `cd ${local}`
 
-    const first_commit_command = await exec(access_repo + " && " + first_commit)
-    const first_commit_sha = first_commit_command.stdout.replace(/(\r\n|\n|\r)/gm, "")
+  const first_commit = 'git rev-list --max-parents=0 HEAD'
+  const access_repo = `cd ${local}`
 
-    return first_commit_sha.trim();
+  const first_commit_command = await exec(access_repo + " && " + first_commit)
+  const first_commit_sha = first_commit_command.stdout.replace(/(\r\n|\n|\r)/gm, "")
+
+  //very uncleanly checking if the first commit is an initial commit
+  //since commits with the message "initail commit" usually don't contain any actual code
+  //and moving on to the next if it is
+  const message = await get_commit_message(first_commit_sha)
+
+  if (message === "first commit" || message === "initial commit") {
+    return await get_next_commit(first_commit_sha)
+  }
+
+  return first_commit_sha.trim();
 
 };
 
 
+/**
+ * Based on a given sha, it returns that commit's child's sha
+ * @param {*} current_sha 
+ */
 async function get_next_commit(current_sha) {
 
   //in case the repo is old, might need to change "main" in this command to "master"
@@ -62,13 +99,17 @@ async function get_next_commit(current_sha) {
   const message = await get_commit_message(next_commit_sha)
 
   //if the next commit is ignored (aka a theory commit), recursively jump to the next non-ignored one
-  if (message.toLowerCase().startsWith("#ignore#") || message.toLowerCase().startsWith("mit license")) { 
+  if (message.toLowerCase().startsWith("#ignore#") || message.toLowerCase().startsWith("mit license")) {
     return get_next_commit(next_commit_sha)
-  } 
+  }
 
   return next_commit_sha.trim()
 }
 
+/**
+ * Based on a given sha, it returns the sha of that commit's parent.
+ * @param {*} commit_sha 
+ */
 async function get_parent(commit_sha) {
   const access_repo = `cd ${local}`
   const get_parent_template = `git rev-parse ${commit_sha}^`
@@ -77,29 +118,39 @@ async function get_parent(commit_sha) {
   const parent_sha = parent_command.stdout.replace(/(\r\n|\n|\r)/gm, "")
 
   const message = await get_commit_message(parent_sha)
-  if (message.toLowerCase().startsWith("#ignore#") || message.toLowerCase().startsWith("#arc#") || message.toLowerCase().startsWith("mit license")) { 
+  if (message.toLowerCase().startsWith("#ignore#") || /*message.toLowerCase().startsWith("#arc#")|| */ message.toLowerCase().startsWith("mit license") ||
+    message.toLowerCase().startsWith("first commit")) {
     return await get_parent(parent_sha)
-  } 
+  }
 
   return parent_sha.trim()
 }
 
 
+/**
+ * Returns a commit's message
+ * In case the commit contains a #theory# part, this will be ignored (cut)
+ * @param {*} commit_sha 
+ */
 async function get_commit_message(commit_sha) {
   const access_repo = `cd ${local}`
 
   const commit_message = `git log --format=%B -n 1 ${commit_sha}`
-  const message_command = await exec (access_repo + " && " + commit_message)
+  const message_command = await exec(access_repo + " && " + commit_message)
   const message = message_command.stdout
 
   return get_only_message(message);
 }
 
 
+/**
+ * Gets diff between two commits
+ * Includes enough context lines to reconstruct the whole changed file
+ * @param {*} commit_sha 
+ */
 async function get_diff(commit_sha) {
 
   const access_repo = `cd ${local}`
-
   const parent_sha = await get_parent(commit_sha)
 
   const get_diff = `git diff -U10000 ${parent_sha} ${commit_sha}`
@@ -109,6 +160,10 @@ async function get_diff(commit_sha) {
   return diff_output;
 }
 
+/**
+ * Special diff case for the first commit, since it doesn't have a parent
+ * @param {*} commit_sha 
+ */
 async function get_diff_first_commit(commit_sha) {
 
   const access_repo = `cd ${local}`
@@ -121,13 +176,20 @@ async function get_diff_first_commit(commit_sha) {
 }
 
 
+
 class TheoryPiece {
   constructor(type, file) {
-      this.type = type;
-      this.file = file;
+    this.type = type;
+    this.file = file;
   }
 }
 
+/**
+ * Retrieves the files describes by the paths in a commit message's #theory# part
+ * Urls are returned as they are
+ * Paths are returned as files
+ * @param {*} commit_sha 
+ */
 async function get_theory(commit_sha) {
   let theory_array = await get_theory_array(commit_sha)
 
@@ -140,12 +202,12 @@ async function get_theory(commit_sha) {
       if (is_url(path)) {
         let theory_piece = new TheoryPiece("URL", path)
         theory.push(theory_piece)
-  
+
       } else {
         let file = await get_file_from_commit(commit_sha, path)
         let type = await detectMimeType(path)
         let theory_piece = new TheoryPiece(type, file)
-        allowed_type(type)? theory.push(theory_piece) : console.log(`Type ${type} not supported`)
+        allowed_type(type) ? theory.push(theory_piece) : console.log(`Type ${type} not supported`)
       }
 
     } catch (error) {
@@ -158,6 +220,12 @@ async function get_theory(commit_sha) {
 }
 
 
+/**
+ * Returns file based on given path
+ * Used to read theory files from the specified theory paths
+ * @param {*} commit_sha 
+ * @param {*} file_path 
+ */
 async function get_file_from_commit(commit_sha, file_path) {
   const access_repo = `cd ${local}`
   let git_checkout = `git checkout ${commit_sha}`
@@ -168,10 +236,11 @@ async function get_file_from_commit(commit_sha, file_path) {
   */
 
   let theory_path = path.join(local, file_path.trim())
-  let file = await readFile(theory_path) 
+  let file = await readFile(theory_path)
 
   return file
 }
+
 
 function allowed_type(mime_type) {
   return (mime_type.startsWith("text/")) || (mime_type.startsWith("image/"))
@@ -182,27 +251,36 @@ function get_only_message(commit_message) {
   return commit_message.split("#theory#")[0].trim()
 }
 
+/**
+ * Returns an array of paths to theory resources specified in a commit message's #theory# part
+ * !!It only returns the urls and paths, not the files!!
+ * @param {*} commit_sha 
+ */
 async function get_theory_array(commit_sha) {
   let theory_array = []
 
   const access_repo = `cd ${local}`
 
   const commit_message = `git log --format=%B -n 1 ${commit_sha}`
-  const message_command = await exec (access_repo + " && " + commit_message)
+  const message_command = await exec(access_repo + " && " + commit_message)
   const message = message_command.stdout
   if (message.includes("#theory#")) {
 
-      let theory = message.split("#theory#")[1]
-      theory_array = theory.split("|")
-      theory_array = theory_array.map(t => t.trim())
-  
+    let theory = message.split("#theory#")[1]
+    theory_array = theory.split("|")
+    theory_array = theory_array.map(t => t.trim())
+
 
   }
-  
-  return theory_array 
+
+  return theory_array
 }
 
 
+/**
+ * Returns the content of the arc description file, added by an arc commit
+ * @param {*} commit_sha 
+ */
 async function arc_description(commit_sha) {
   const access_repo = `cd ${local}`
 
@@ -222,14 +300,20 @@ async function arc_description(commit_sha) {
 }
 
 
+/**
+ * Get paths to files that have been changed by a particular commit
+ * Mostly used to get the path to the arc.txt file added by an arc commit
+ * Since an arc commit makes no other changes, this function returns only this file
+ * @param {*} commit_sha 
+ */
 async function get_new_files_paths(commit_sha) {
   const access_repo = `cd ${local}`
 
-  const get_added_file_paths = `git show --pretty="" ${commit_sha} --name-only`  
+  const get_added_file_paths = `git show --pretty="" ${commit_sha} --name-only`
   const added_paths_command = await exec(access_repo + " && " + get_added_file_paths)
 
   let added_file_paths = added_paths_command.stdout.split("\n")
-  added_file_paths = added_file_paths.slice(0, added_file_paths.length-1)
+  added_file_paths = added_file_paths.slice(0, added_file_paths.length - 1)
 
   return added_file_paths
 }
@@ -237,11 +321,11 @@ async function get_new_files_paths(commit_sha) {
 async function detectMimeType(filePath) {
   let absolute_path = path.join(local, filePath)
   let detectMime = new Promise((resolve, reject) => {
-        var magic = new Magic(mmm.MAGIC_MIME_TYPE);
-        magic.detectFile(absolute_path, function(err, result) {
-             if (err) reject(err);
-             resolve(result);
-        });
+    var magic = new Magic(mmm.MAGIC_MIME_TYPE);
+    magic.detectFile(absolute_path, function (err, result) {
+      if (err) reject(err);
+      resolve(result);
+    });
   });
 
   let result = await detectMime;
@@ -253,6 +337,6 @@ function is_url(string) {
   let regexp = /(ftp|http|https):\/\/(\w+:{0,1}\w*@)?(\S+)(:[0-9]+)?(\/|\/([\w#!:.?+=&%@!\-\/]))?/
   return regexp.test(string);
 }
- 
 
-module.exports={ commit_exists, get_first_commit, get_next_commit, get_parent, get_commit_message, arc_description, get_diff, get_diff_first_commit, get_theory, get_theory_array, get_file_from_commit}
+
+module.exports = { clone_repo, commit_exists, get_first_commit, get_next_commit, get_parent, get_commit_message, arc_description, get_diff, get_diff_first_commit, get_theory, get_theory_array, get_file_from_commit }
